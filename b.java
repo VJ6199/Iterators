9d7ccaf56222262192c352ab522be55db92d6f09
vj6199 <vijaykrishna6199@gmail.com>
2022-08-24 23:19:44 -0400

    dc

---
+++ b.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class b {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        TreeSet<String> sree = new TreeSet<>(); //Ascending Order
+        System.out.println(sree.isEmpty());      //isEmpty - true
+
+        sree.add("e");
+        sree.add("c");
+        sree.add("a");
+        sree.add("d");
+        sree.add("f");
+        sree.add("b");
+        sree.add(" ");
+
+        //System.out.println(sree);
+        Iterator<String> i = sree.iterator();
+        System.out.println(i);
+        while(i.hasNext()) {
+            String s = i.next();
+            System.out.println(s);
+        }
+}
+}
